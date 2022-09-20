a34dacbb76617645053464aef6fafec938bae089
thiago <thiagoe99@hotmail.com>
2022-09-20 18:02:59 -0300

    alteracao link api get url original

--- Front-end/encurtadorurlflutter/lib/class/getUrlOriginal.class.dart
+++ Front-end/encurtadorurlflutter/lib/class/getUrlOriginal.class.dart
@@ -2,16 +2,15 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 Future<Map<String, dynamic>> getUrlOriginal(String url) async {
-  // final queryParams = <String, String>{'token': token};
+  final queryParams = <String, String>{'url_encurtada': url};
 
-  // final uriDF = Uri.https(
-  //     'southamerica-east1-encurtador-url-thg.cloudfunctions.net',
-  //     '/geturlsuser',
-  //     queryParams);
+  final uriDF = Uri.https(
+      'southamerica-east1-encurtador-url-thg.cloudfunctions.net',
+      '/geturloriginal',
+      queryParams);
 
   final response = await http.get(
-    Uri.parse('http://127.0.0.1:2020?url_encurtada='+ url),
-    // uriDF,
+    uriDF,
     headers: <String, String>{
       'Access-Control-Allow-Origin': '*',
       'Content-Type': 'application/json'
